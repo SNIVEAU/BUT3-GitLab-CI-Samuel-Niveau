69b7866efb0400bf685ae8e7e9a192b80f0dfd4c
SNIVEAU <odevva@gmail.com>
2025-09-08 15:28:08 +0200

    correction pylint

--- src/greeting/main.py
+++ src/greeting/main.py
@@ -1,7 +1,5 @@
-def greet(name): 
-    return f"Hello, {name}!" 
+def greet(name):
+    return f"Hello, {name}!"
     
-if __name__ == "__main__": 
-    print(greet("GitLab CI/CD")) 
-
-    
+if __name__ == "__main__":
+    print(greet("GitLab CI/CD"))
